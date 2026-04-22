abad5e03d9ed94a14fe8230233d6da8e1706a015
Tel3a <115263186+Tel3a@users.noreply.github.com>
2026-04-22 12:10:06 +0000

    gg

---
+++ main.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<title>Mein Biber Webshop</title>
+<link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+	
+   
+<div class="topnav">
+	<div id="home"><a href="startseite.php"><img src="Symbol.gif" height="100px" alt="Home"> </a></div>
+	<br>
+	<!--
+   <div class="Leiste"><a href="LAREGION.html">LA REGION </a>||
+	<a href="LESACTIVITES.html">LES ACTIVITES </a>||
+	<a href="LEPLAN.html">LE PLAN </a>||
+	<a href="SURNOUS.html">SUR NOUS </a></div>
+   -->
+</div>
+<div class="pfeil"> <a href="startseite.php"> <b>↑</b> </a></div>
+   
+   <h1>Willkommen auf meiner Webseite!</h1>
+   <p>Dies ist ein einfacher Absatz.</p>
+<div class="item" id="item4"> 
+			<div id="footerinhalt"><br>© 2026 Jamie-Lee Jones, Telsa Schaurer, Sophie Gorqaj <br> <br> 	<a href="Impressum.html">IMPRESSUM</a> <br>  <a href="Quellen.html">QUELLEN</a> <br> </div>
+</div>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
