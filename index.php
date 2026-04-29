7784bffabc33bdf87611a01cee6af07b5fdc2e9d
Tel3a <115263186+Tel3a@users.noreply.github.com>
2026-04-29 14:21:35 +0000

    test

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 
 <div class="pfeil"> <a href="startseite.php"> <b>↑</b> </a></div>
    
-   <h1>Willkommen auf meiner Webseite!</h1>
+   <h1>Willkommen auf diesem Webshop!</h1>
    <p>Dies ist ein einfacher Absatz.</p>
 <div class="item" id="item4"> 
 			<div id="footerinhalt"><br>© 2026 Jamie-Lee Jones, Telsa Schaurer, Sophie Gorqaj <br> <br> 	<a href="Impressum.html">IMPRESSUM</a> <br>  <a href="Quellen.html">QUELLEN</a> <br> </div>
