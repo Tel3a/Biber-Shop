f3ca763eab2743c357e32a3a080e6ef09bd5b254
GalaxyUni23 <133406143+GalaxyUni23@users.noreply.github.com>
2026-05-07 12:12:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -21,6 +21,26 @@
 			<div id="footerinhalt"><br>© 2026 Jamie-Lee Jones, Telsa Schaurer, Sophie Gorqaj <br> <br> 	<a href="Impressum.html">IMPRESSUM</a> <br>  <a href="Quellen.html">QUELLEN</a> <br> </div>
 </div>
 
+        <h1>Shop und Produkte für Biber</h1> 
+	<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "password";
+	$datenbank = "Bibershop";
+
+	// Verbindung herstellen
+	$conn = mysqli_connect($servername, $username, $password, $datenbank);
+	
+	//Daten abfragen
+	$sql = "SELECT * FROM Produkte";
+	$result = mysqli_query($conn, $sql);
+
+	while($row = mysqli_fetch_assoc($result)) {
+	    echo "id: " . $row["PID"]. " - Name: " . $row["PName"]. " - Beschreibung: " . $row["PBeschreibung"]. " - Preis: ".$row["PPreis"]. " - Bestand: ".$row["PBestand"]."<br>";
+	  }
+	
+	 mysqli_close($conn); 
+	?> 
 
 
 
