748c20bef18779c9a06d2dbaffc543ad21793141
GalaxyUni23 <133406143+GalaxyUni23@users.noreply.github.com>
2026-05-15 11:30:38 +0200

    Login Funktion
    Nur die Felder sind da, noch nicht mit Datenbank verknüpft, dh noch keine Registrierung oä möglich

--- login.php
+++ login.php
@@ -18,6 +18,32 @@
 
 <div class="pfeil"> <a href="login.php"> <b>↑</b> </a></div>
 
+<div class="login">
+	<?php
+        $texttext = "Hallo! ";
+        $error = "";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (empty($_POST["name"])) {
+                $error = "ungültiger Name";
+            }
+            elseif (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+                $error = "ungültige E-mail";
+            }
+            else {
+                echo "<h1> Hallo " . $_POST["name"] . ", viel Spaß beim Kaufen! </h1>";
+                echo "<h3> Liebe/r " . $_POST["name"] . ", deine Emailadresse lautet:" . $_POST["email"] . "</h3>";
+            }
+        }
+    ?>
+ 
+	<h1>Login</h1>
+	<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method ="post">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <?php echo $error ?>
+        <input type="submit">
+    </form>
+</div>
           
 <div class="item" id="footer"> 
 			<div id="footerinhalt"><br>© 2026 Jamie-Lee Jones, Telsa Schaurer, Sophie Gorqaj <br> <br> 	<a href="Impressum.html">IMPRESSUM</a> <br>  </div>
