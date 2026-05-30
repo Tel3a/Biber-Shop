810ceb107baa44ba0446fd061e38d85ca554f833
GalaxyUni23 <133406143+GalaxyUni23@users.noreply.github.com>
2026-05-30 16:51:22 +0200

    Login Funktion

--- login.php
+++ login.php
@@ -7,50 +7,44 @@
 </head>
 
 <body> 
- <div class="menuband">
-	<a id="logo" href="index.php"><img src="house.png" alt="Startseite" ></a>
-	<div menuoptionen> <a  href="kaufen.php">Kaufen</a>
-	<a href="login.php">Login</a> </div>
-</div>
 
-<div class="seiteninhalt">
-
-
-
-<div class="login">
-	<?php
-        $texttext = "Hallo! ";
-        $error = "";
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if (empty($_POST["name"])) {
-                $error = "ungültiger Name";
+<div class ="container">
+    <div class="form-box" id="login-form">
+	    <?php
+            $texttext = "Hallo! ";
+            $error = "";
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+                    $error = "<h5>" . "ungültige E-mail" . "</h5>";
+                }
+                elseif (empty($_POST["passwort"])) {
+                    $error = "<h5>" . "ungültiges Passwort" . "</h5>";
+                }
+                else {
+                    header("Location: index.php");
+                    exit();
+                }
             }
-            elseif (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-                $error = "ungültige E-mail";
-            }
-            else {
-                echo "<h1> Hallo " . $_POST["name"] . ", viel Spaß beim Kaufen! </h1>";
-                echo "<h3> Liebe/r " . $_POST["name"] . ", deine Emailadresse lautet:" . $_POST["email"] . "</h3>";
-            }
-        }
-    ?>
+        ?>
  
-	<h1>Login</h1>
-	<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method ="post">
-        E-mail <br><input type="text" name="email"><br>
-        Benutzername <br><input type="text" name="name"><br>
-        Passowort <br><input type="password" name="passwort"><br>
-        <?php echo $error ?> <br>
-        <input type="submit">
-    </form>
+    	<h1>Login</h1>
+    	<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method ="post">
+            <input type="text" name="email" placeholder="Email" required><br>
+            <input type="password" name="passwort" placeholder="Passwort" required><br>
+            <button type="submit">Login</button><br>
+            <p>Noch kein Konto? <a href="registrieren.php">Hier registrieren</a></p>
+            <?php echo $error ?>
+
+        </form>
+    </div>
+    
 </div>
 
-</div>         
-
+<!--
 <div class="item" id="footer"> 
 			<div id="footerinhalt"><br>© 2026 Jamie-Lee Jones, Telsa Schaurer, Sophie Gorqaj <br> <br> 	<a href="Impressum.html">IMPRESSUM</a> <br>  </div>
 </div>
-
+-->
 
 </body>
 </html>
