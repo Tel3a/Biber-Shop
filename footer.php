7047c64ea724ce08eacca802f2f7856c715d35fc
Tel3a <115263186+Tel3a@users.noreply.github.com>
2026-05-31 20:43:58 +0200

    Update footer with additional contact information

--- footer.php
+++ footer.php
@@ -1,8 +1,14 @@
-<div class="item" id="footer"> 
-	<div id="footerinhalt"> 
-        <h4>KONTAKT</h4>
-        <p>Telefon: +49 123 456 789</p>
-        <p>Email: info@bibershop.de</p>
-        © 2026 Jamie-Lee Jones, Telsa Schaurer, Sophie Gorqaj  </div>  
+<div id="footer"> 
+        <div class ="footerinhalt" class="kontakt"><h4>KONTAKT</h4>
+        Telefon: +49 123 456 789
+        Email: info@bibershop.de
+        Adresse: Biberstraße 1, 12345 Biberstadt
+        </div>
+        <div class ="footerinhalt" class="infos"><h4>INFO</h4>
+        Telefon: +49 123 456 789
+        Email: info@bibershop.de
+        Adresse: Biberstraße 1, 12345 Biberstadt
+        </div>
+        © 2026 Jamie-Lee Jones, Telsa Schaurer, Sophie Gorqaj 
 </div>
 
