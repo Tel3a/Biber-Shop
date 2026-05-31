985d8d66ccd9c64c233fb51d4e3f6d0f0760998d
Tel3a <115263186+Tel3a@users.noreply.github.com>
2026-05-31 19:49:50 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,27 +1,19 @@
- <!DOCTYPE html>
-<html lang="de">
-<head>
-  <meta charset="utf-8">
-  <title>Mein Biber Webshop</title>
-  <link href="style.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
-</head>
 
-<body> 
 	
-<div class="header">
+<!--<div class="header">
 	<a id="logo" href="index.php"><img src="biber.svg" alt="Startseite" ></a>
 	<div class="menuoptionen"> <a  href="kaufen.php">Kaufen</a>
 	<a href="login.php">Konto</a> </div>
 	<a href="warenkorb.php">Warenkorb</a>
-</div>
-
-<!-- 	<a id="logo" href="index.php"><img src="biber.svg" alt="Startseite" ></a>
+</div>-->
+<div class="header">
+ 	<a id="logo" href="index.php"><img src="biber.svg" alt="Startseite" ></a>
 	<div class="menuoptionen"> 
-  <a  href="kaufen.php">Kaufen</a>
+  <a href="kaufen.php">Kaufen</a>
 	<a href="login.php"><img src="konto.svg" alt="log-in" ></a>
   <a href="warenkorb.php"><img src="warenkorb.svg" alt="Startseite" ></a>
- */-->
+ </div>
+ </div>
 
-</body>
-</html>
+
+
