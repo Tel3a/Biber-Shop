990b2e1dc89c5abbffe85304e8e3d4b9f0d9e16a
Tel3a <115263186+Tel3a@users.noreply.github.com>
2026-05-31 20:45:23 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 <div id="footer"> 
-        <div class ="footerinhalt" class="kontakt"><h4>KONTAKT</h4>
-        Telefon: +49 123 456 789
-        Email: info@bibershop.de
-        Adresse: Biberstraße 1, 12345 Biberstadt
-        </div>
-        <div class ="footerinhalt" class="infos"><h4>INFO</h4>
-        Telefon: +49 123 456 789
-        Email: info@bibershop.de
-        Adresse: Biberstraße 1, 12345 Biberstadt
-        </div>
-        © 2026 Jamie-Lee Jones, Telsa Schaurer, Sophie Gorqaj 
+<div class ="footerinhalt" class="kontakt"><h4>KONTAKT</h4>
+    Telefon: +49 123 456 789
+    Email: info@bibershop.de
+    Adresse: Biberstraße 1, 12345 Biberstadt
+    </div>
+    <div class ="footerinhalt" class="infos"><h4>INFO</h4>
+    Telefon: +49 123 456 789
+    Email: info@bibershop.de
+    Adresse: Biberstraße 1, 12345 Biberstadt
+</div>
+    © 2026 Jamie-Lee Jones, Telsa Schaurer, Sophie Gorqaj 
 </div>
 
