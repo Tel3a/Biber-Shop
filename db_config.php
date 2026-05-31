7ef3f2169604c2e5cb91c677e3fbd45a90e72dd6
GalaxyUni23 <133406143+GalaxyUni23@users.noreply.github.com>
2026-05-31 19:52:41 +0200

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,5 +1,15 @@
 <?php
-$conn = new mysqli('localhost','root','','Bibershop');
-if ($conn->connect_error) die('DB-Fehler: '.$conn->connect_error);
-$conn->set_charset('utf8mb4');
+    function get_db_connection(): mysqli {
+        static $conn = null;
+        if ($conn === null) {
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "bibershop";
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            if ($conn->connect_error) die('DB-Fehler: '.$conn->connect_error);
+            $conn->set_charset('utf8mb4');
+        }
+        return $conn;
+    }
 ?>
