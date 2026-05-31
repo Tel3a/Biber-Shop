bce873d344cf6dbfa35aac62aa6718528a78722e
Tel3a <115263186+Tel3a@users.noreply.github.com>
2026-05-31 16:54:19 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,21 @@
+ <!DOCTYPE html>
+<html lang="de">
+<head>
+  <meta charset="utf-8">
+  <title>Mein Biber Webshop</title>
+  <link href="style.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+</head>
+
+<body> 
+
+<div class="item" id="footer"> 
+	<div id="footerinhalt"> 
+        <h4>KONTAKT</h4>
+        <p>Telefon: +49 123 456 789</p>
+        <p>Email: info@bibershop.de</p>
+        © 2026 Jamie-Lee Jones, Telsa Schaurer, Sophie Gorqaj  </div>  
+</div>
+
+</body>
+</html>
