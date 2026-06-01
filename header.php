c178f21f9a3691ca5c7e6a2ae9e09b5bdb4f51b5
Tel3a <115263186+Tel3a@users.noreply.github.com>
2026-06-01 23:52:39 +0200

    Update header.php

--- header.php
+++ header.php
@@ -23,6 +23,44 @@
  <div id="mySidenav" class="sidenav">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
   <a href="kaufen.php">Zum Shop</a>
+  <a href="ueberuns.php">Über uns</a>
+
+
+</div>
+
+
+
+
+
+<!-- <script src="script.js"></script> -->
+
+</body>
+</html>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <meta charset="utf-8">
+  <title>Mein Biber Webshop</title>
+  <link href="style.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+</head>
+
+<body> 
+    
+<div class="header">
+    <a id="logo" href="index.php"><img src="biber.svg" alt="Startseite" ></a>
+    <div class="menuoptionen"> 
+        <a href="login.php"><img src="konto.svg" alt="log-in" ></a>
+        <a href="warenkorb.php"><img src="warenkorb.svg" alt="Startseite" ></a>
+        <img id="menu" src="menu.svg" alt="Menü"> 
+    </div>
+ </div>
+
+
+
+ <div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="kaufen.php">Zum Shop</a>
   <a href="#">Über uns</a>
 
   <ul>
