f94f3292955ae9f038a390e4ffd7e1d09f8ab87b
Tel3a <115263186+Tel3a@users.noreply.github.com>
2026-06-01 17:35:57 +0200

    Refactor header structure and add sidenav

--- header.php
+++ header.php
@@ -1,19 +1,38 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <meta charset="utf-8">
+  <title>Mein Biber Webshop</title>
+  <link href="style.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+</head>
 
-	
-<!--<div class="header">
-	<a id="logo" href="index.php"><img src="biber.svg" alt="Startseite" ></a>
-	<div class="menuoptionen"> <a  href="kaufen.php">Kaufen</a>
-	<a href="login.php">Konto</a> </div>
-	<a href="warenkorb.php">Warenkorb</a>
-</div>-->
+<body> 
+    
 <div class="header">
- 	<a id="logo" href="index.php"><img src="biber.svg" alt="Startseite" ></a>
-	<div class="menuoptionen"> 
-  <a href="kaufen.php">Kaufen</a>
-	<a href="login.php"><img src="konto.svg" alt="log-in" ></a>
-  <a href="warenkorb.php"><img src="warenkorb.svg" alt="Startseite" ></a>
+    <a id="logo" href="index.php"><img src="biber.svg" alt="Startseite" ></a>
+    <div class="menuoptionen"> 
+        <a href="login.php"><img src="konto.svg" alt="log-in" ></a>
+        <a href="warenkorb.php"><img src="warenkorb.svg" alt="Startseite" ></a>
+        <img id="menu" src="menu.svg" alt="Menü"> 
+    </div>
  </div>
- </div>
 
 
 
+ <div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="kaufen.php">Zum Shop</a>
+  <a href="#">Über uns</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+</div>
+
+
+
+
+
+<!-- <script src="script.js"></script> -->
+
+</body>
+</html>
